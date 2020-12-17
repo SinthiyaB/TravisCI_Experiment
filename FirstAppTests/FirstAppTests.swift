cc45b71f6c7517b596ade9baa2b7edea05838aaa
SinthiyaB <44456810+SinthiyaB@users.noreply.github.com>
2020-12-17 20:23:06 +0530

    Update FirstAppTests.swift

--- FirstAppTests/FirstAppTests.swift
+++ FirstAppTests/FirstAppTests.swift
@@ -25,11 +25,11 @@ class FirstAppTests: XCTestCase {
     
     func testConversionForOne() {
       let result = converter.convert(1)
-      XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
+      XCTAssertEqual(result, "I", "Conversion for 1 is incorrect..")
     }
     
     func testConversionForTwo() {
       let result = converter.convert(2)
-      XCTAssertEqual(result, "II", "Conversion for 2 is incorrect")
+      XCTAssertEqual(result, "II", "Conversion for 2 is incorrect..")
     }
 }
